d9d9be2e66def09d96c8558f66b8b61a0d5e2367
Ivan Patsai <ivan.patsai@binary-studio.com>
2018-05-24 10:51:34 +0300

    wrong commit

--- src/store/TodoHistory.js
+++ src/store/TodoHistory.js
@@ -16,7 +16,7 @@ class TodoHistory {
 	fetchTodoHistory (id) {
 		axios.get('https://stormy-castle-67867.herokuapp.com/api/Todo_Histories?filter[where][todoId]=' + id).then((res) => {
 			console.log(res);
-			this.todoHistory = [...res.data];
+			this. tadoHistory = [...res.data];
 		});
 	}
 
